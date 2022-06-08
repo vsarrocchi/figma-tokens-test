41df7b4129c8c33f4ce332959ab2aaae17847de7
Valesca Sarrocchi <valescasarrocchi@Valescas-MacBook-Pro.local>
2022-06-08 11:40:29 +0200

    added tokens

--- styles/design-tokens.js
+++ styles/design-tokens.js
@@ -1,7 +1,13 @@
 /**
  * Do not edit directly
- * Generated on Wed, 08 Jun 2022 09:23:55 GMT
+ * Generated on Wed, 08 Jun 2022 09:39:13 GMT
  */
 
 export const tokenLarge = "1.25rem";
 export const tokenPinkred = "#f00";
+export const tokenShadowtestX = 10;
+export const tokenShadowtestY = 10;
+export const tokenShadowtestBlur = 10;
+export const tokenShadowtestSpread = 10;
+export const tokenShadowtestColor = "#000000";
+export const tokenShadowtestType = "dropShadow";
